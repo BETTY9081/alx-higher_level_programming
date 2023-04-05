933a7395df41419b33c189c50ae41b99b4d9c398
BETTY9081 <bettykebede1981@gmail.com>
2023-04-05 01:11:45 -0700

    moree classes

---
+++ 0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+Rectangle = __import__('0-rectangle').Rectangle	
+"""Defines a Rectangle class."""
+
+
+class Rectangle:
+    """Represent a rectangle."""
+    pass
+0 comments on commit 32373d8
